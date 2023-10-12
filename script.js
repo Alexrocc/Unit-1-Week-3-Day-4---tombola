1c80df70f7ecd476b8c09fc964f715fa4a7c4847
Alessio Rocchigiani <alerocc70@gmail.com>
2023-10-12 18:04:18 +0200

    document formatted (prettier was off)

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // CREAZIONE CELLE
 
+  const cellCreation = function () {
+    for (let i = 0; i < 90; i++) {
+      const tableSection = document.getElementById("tableSection");
 
-// CREAZIONE CELLE
+      const newCell = document.createElement("div");
+      newCell.classList.add("cell");
 
-const cellCreation = function(){
-    
-        for (let i = 0; i < 90; i++) {
-    
-            const tableSection = document.getElementById("tableSection")
-    
-            const newCell = document.createElement("div")
-            newCell.classList.add("cell")
-    
-            const newCellNum = document.createElement("h4")
-            newCellNum.innerText = i + 1
-            newCellNum.classList.add("cellH4")
-    
-            const checkedDot = document.createElement("div")
-            checkedDot.classList.add("cellDot")
-    
-            newCell.appendChild(newCellNum)
-    
-            newCell.appendChild(checkedDot)
-    
-            tableSection.appendChild(newCell)
-        }}
-        cellCreation()
-    
-        // RANDOMIZZAZIONE NUMERO DA BUTTON
-    
-        const randomNumBtn = document.getElementById("extractionBtn") 
-    
-        randomNumBtn.onclick = function () {
-            const extraction = Math.floor(Math.random() * (90 - 1) + 1); 
-    
-            const dot = document.querySelectorAll(".cellDot")
-            const dotArray = Array.from(dot)
-    
-            dotArray[extraction - 1].classList.add("checkedNumber")
-        }
-    })
+      const newCellNum = document.createElement("h4");
+      newCellNum.innerText = i + 1;
+      newCellNum.classList.add("cellH4");
+
+      const checkedDot = document.createElement("div");
+      checkedDot.classList.add("cellDot");
+
+      newCell.appendChild(newCellNum);
+
+      newCell.appendChild(checkedDot);
+
+      tableSection.appendChild(newCell);
+    }
+  };
+  cellCreation();
+
+  // RANDOMIZZAZIONE NUMERO DA BUTTON
+
+  const randomNumBtn = document.getElementById("extractionBtn");
+
+  randomNumBtn.onclick = function () {
+    const extraction = Math.floor(Math.random() * (90 - 1) + 1);
+
+    const dot = document.querySelectorAll(".cellDot");
+    const dotArray = Array.from(dot);
+
+    dotArray[extraction - 1].classList.add("checkedNumber");
+  };
+});
